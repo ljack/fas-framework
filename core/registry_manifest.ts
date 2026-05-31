ac645fcaa8311c85dfb42c0343df66d731a14f31
Jarkko Lietolahti <Jarkko.lietolahti@gmail.com>
2026-06-01 00:35:38 +0300

    Implement Leaderboard & Timer feature on Sudoku application

--- core/registry_manifest.ts
+++ core/registry_manifest.ts
@@ -2,7 +2,9 @@
 import { FeatureRegistry } from './registry';
 
 import { exampleFeature } from '../features/example/feature';
+import { leaderboardFeature } from '../features/leaderboard/feature';
 
 export function registerActiveFeatures(registry: FeatureRegistry) {
   registry.register(exampleFeature);
+  registry.register(leaderboardFeature);
 }
